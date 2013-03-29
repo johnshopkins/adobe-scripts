488974ba94c68d4a01632fe4fb63b2fbc85fd2a3
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2013-03-29 18:53:51 -0400

    Moved the polyfill prototype additions to the top, grouped

--- Illustrator/JHU-Logo-Exporter.jsx
+++ Illustrator/JHU-Logo-Exporter.jsx
@@ -6,17 +6,35 @@
 *
 */
 
+/**
+ * Adobe JS Polyfills 
+ * 
+ * Could be moved to a separate file if we could figure
+ * out how to include other script files
+ */
+Folder.prototype.insert = function (name) {
+    var folder = new Folder(this.fsName + "/" + name);
+    folder.create();
+    return folder;
+};
+
+Array.prototype.map = function (callback) {
+    var newList = [];
+    for (i=0; i<this.length; i++) {
+        newList.push(callback(this[i]));
+    }
+    return newList;
+};
+
+String.prototype.trim = function () {
+    return this.replace(/^\s+|\s+$/g, '');
+};
+
 
 var doc = app.activeDocument;
 
 // var savePath = Folder.selectDialog("Choose the folder where these packs will be saved.");
 var absPath = doc.path.fsName;
-
-Folder.prototype.insert = function (name) {
-	var folder = new Folder(this.fsName + "/" + name);
-	folder.create();
-	return folder;
-};
 
 var allPacks = doc.path.insert("All Packs");
 
@@ -137,18 +155,6 @@ function forEach(collection, fn)
 	{
 		fn(collection[i]);
 	}
-}
-
-Array.prototype.map = function (callback) {
-	var newList = [];
-	for (i=0; i<this.length; i++) {
-		newList.push(callback(this[i]));
-	}
-	return newList;
-};
-
-String.prototype.trim = function () {
-	return this.replace(/^\s+|\s+$/g, '');
 }
 
 var processAttributes = function (item) {
