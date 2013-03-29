2aa81f3e1ceb174e47343da9a743a159237d803a
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2013-03-29 18:41:43 -0400

    Changed the description text

--- Illustrator/JHU-Logo-Exporter.jsx
+++ Illustrator/JHU-Logo-Exporter.jsx
@@ -1,25 +1,9 @@
 /*
-* Description: An Adobe Illustrator script that export each layer as a separate PNG image file
-* Usage: Layer name is the PNG file name. Rename layers if necessary.
-* This is an early version that has not been sufficiently tested. Use at your own risks.
-* License: GNU General Public License Version 3. (http://www.gnu.org/licenses/gpl-3.0-standalone.html)
+* Description: 
+* 
+* An Adobe Illustrator CS6 script to auto export files
+* first by artboard and then by name-matched layer.
 *
-* Copyright (c) 2009. William Ngan.
-* http://www.metaphorical.net
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 */
 
 
